7f66df5573e42c9c1d51bdcefe4ce55d425e9bce
Bruno Fernandez <brunofernandez@msn.com>
2018-03-18 11:38:44 +0100

    randomizing exercise

--- src/Hassenstein/Exercises3/Randomizing.java
+++ src/Hassenstein/Exercises3/Randomizing.java
@@ -1,11 +1,14 @@
 package Hassenstein.Exercises3;
 
+import java.security.SecureRandom;
+import java.security.Security;
 import java.util.Random;
 
 public class Randomizing {
-    //create random bytes with java Random
     public static void main(String[] args) {
+        //create random bytes with java Random
         Random myrand = new Random();
+        //by using same seed, the bytecode will be everytime the same
         //myrand.setSeed(12542745);
 
         byte[] mybytes = new byte[256];
@@ -13,6 +16,17 @@ public class Randomizing {
         myrand.nextBytes(mybytes);
         for (int i = 0; i< mybytes.length; i++){
             System.out.println(mybytes[i]);
+        }
+
+        //create random bytes with Java SecureRandom using Security providers
+        SecureRandom mysecrand = new SecureRandom();
+        System.out.println(Security.getProviders());
+
+        byte[] secybytes = new byte[256];
+        mysecrand.nextBytes(secybytes);
+
+        for (int i = 0; i< secybytes.length; i++){
+            System.out.println(secybytes[i]);
         }
     }
 }
